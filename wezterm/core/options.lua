6324b3c5ce97a6a10bab221c8f39e5af21e40579
Ugolin O. <hello@ugolin-olle.com>
2025-01-10 13:54:05 +0700

    feat(wezterm): adding multiple fonts

--- wezterm/core/options.lua
+++ wezterm/core/options.lua
@@ -2,22 +2,22 @@ local wezterm = require 'wezterm'
 
 local M = {
   font_size = 12.5,
-  line_height = 1.2,
-  font = wezterm.font("CommitMono"),
+  line_height = 1.4,
+  font = wezterm.font_with_fallback({ { family = "CommitMono" }, { family = "JetbrainsMono Nerd Font" } }),
   window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
   default_cursor_style = 'BlinkingUnderline',
   colors = require("themes.cyberdream"),
   window_padding = {
-    left = 40,
-    right = 40,
-    top = 40,
-    bottom = 40,
+    left = 20,
+    right = 20,
+    top = 20,
+    bottom = 20,
   },
   max_fps = 120,
   prefer_egl = true,
-  window_background_opacity = 0.9,
-  macos_window_background_blur = 30
+  window_background_opacity = 0.6,
+  macos_window_background_blur = 20
 }
 
 return M
