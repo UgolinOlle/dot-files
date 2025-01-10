cab1c99332e5ad21cbd249a2e4d249fbe835b045
Ugolin O. <hello@ugolin-olle.com>
2025-01-10 12:54:12 +0700

    refactor(core): improve & refacto init

--- wezterm/core/init.lua
+++ wezterm/core/init.lua
@@ -8,4 +8,25 @@ M.events = require("core.events")
 M.mappings = require("core.mappings")
 M.launcher = require("core.launcher")
 
+function M.init_term()
+  M.status.enable()
+
+  local modules = M.utils.map({
+    'core.options',
+    'core.events',
+    'core.mappings',
+    'core.tab',
+    'core.launcher',
+  }, M.utils.req)
+
+  local valid_modules = {}
+  for _, module in ipairs(modules) do
+    if type(module) == 'table' then
+      table.insert(valid_modules, module)
+    end
+  end
+
+  return M.utils.merge(table.unpack(valid_modules))
+end
+
 return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,3 @@
 local core = require("core");
 
-core.status.enable()
-
-local modules = core.utils.map({
-  'core.options',
-  'core.events',
-  'core.mappings',
-  'core.tab',
-  'core.launcher',
-}, core.utils.req)
-
-local valid_modules = {}
-for _, module in ipairs(modules) do
-  if type(module) == 'table' then
-    table.insert(valid_modules, module)
-  end
-end
-
-return core.utils.merge(table.unpack(valid_modules))
+return core.init_term()
