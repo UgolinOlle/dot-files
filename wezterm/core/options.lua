67a04525675b3afc205266b3cbfd72c0c33e9018
Ugolin O. <hello@ugolin-olle.com>
2025-01-30 12:47:47 +0700

    feat(wezterm): adding new options on hover, switch panes

--- wezterm/core/options.lua
+++ wezterm/core/options.lua
@@ -1,24 +1,25 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local M = {
-  font_size = 12.5,
-  line_height = 1.4,
-  font = wezterm.font_with_fallback({ { family = "CommitMono" }, { family = "JetbrainsMono Nerd Font" } }),
-  window_decorations = "RESIZE",
-  hide_tab_bar_if_only_one_tab = true,
-  default_cursor_style = 'BlinkingUnderline',
-  -- colors = require("themes.cyberdream"),
-  color_scheme = "Abernathy",
-  window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 20,
-  },
-  max_fps = 120,
-  prefer_egl = true,
-  window_background_opacity = 0.6,
-  macos_window_background_blur = 20
+	font_size = 12.5,
+	line_height = 1.4,
+	font = wezterm.font_with_fallback({ { family = "CommitMono" }, { family = "JetbrainsMono Nerd Font" } }),
+	window_decorations = "RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
+	default_cursor_style = "BlinkingUnderline",
+	-- colors = require("themes.cyberdream"),
+	color_scheme = "Abernathy",
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
+	max_fps = 120,
+	prefer_egl = true,
+	window_background_opacity = 0.6,
+	macos_window_background_blur = 20,
+	pane_focus_follows_mouse = true,
 }
 
 return M
